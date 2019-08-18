ff709e39869fb2571ecf3369263b2f95d41661fe
Timothy Wrona <tjwrona1992@gmail.com>
2019-08-18 12:36:16 -0400

    Update fft.cpp
    Improved the bit reversal algorithm and made variables const that never change

--- src/fft.cpp
+++ src/fft.cpp
@@ -1,5 +1,6 @@
 #include "fft.h"
 
+#include <bitset>
 #include <cassert>
 #include <complex>
 #include <vector>
@@ -9,16 +10,17 @@ namespace opendsp
 
 static std::vector<std::complex<double>> FFTRadix2(const std::vector<std::complex<double>>& x, const std::vector<std::complex<double>>& W);
 static bool IsPowerOf2(size_t x);
-static size_t ReverseBits(const size_t x, const size_t n);
+
+static constexpr size_t wordSize = sizeof(size_t) * 8;
 
 std::vector<std::complex<double>> FFT(const std::vector<double>& x)
 {
-    size_t N = x.size();
+    const size_t N = x.size();
 
     // Radix2 FFT requires length of the input signal to be a power of 2
     // TODO: Implement other algorithms for when N is not a power of 2
     assert(IsPowerOf2(N));
-    size_t NOver2 = N / 2;
+    const size_t NOver2 = N / 2;
 
     // Taking advantage of symmetry the FFT of a real signal can be computed
     // using a single N/2-point complex FFT. Split the input signal into its
@@ -27,16 +29,17 @@ std::vector<std::complex<double>> FFT(const std::vector<double>& x)
     for (size_t n = 0; n < NOver2; ++n)
     {
         // x_p[n] = x[2n] + jx[2n + 1]
-        auto nTimes2 = n * 2;
+        const auto nTimes2 = n * 2;
         x_p[n] = std::complex<double>(x[nTimes2], x[nTimes2 + 1]);
     }
 
     // Pre-calculate twiddle factors
     std::vector<std::complex<double>> W(NOver2);
     std::vector<std::complex<double>> W_p(NOver2 / 2);
+    const auto twiddleConstant = -2.0 * M_PI / N;
     for (size_t k = 0; k < NOver2; ++k)
     {
-        W[k] = std::polar(1.0, -2 * M_PI * k / N);
+        W[k] = std::polar(1.0, k * twiddleConstant);
 
         // The N/2-point complex DFT uses only the even twiddle factors
         if (k % 2 == 0)
@@ -53,20 +56,20 @@ std::vector<std::complex<double>> FFT(const std::vector<double>& x)
     X[0] = X_p[0].real() + X_p[0].imag();
     for (size_t k = 1; k < NOver2; ++k)
     {
-        auto l = NOver2 - k;
+        const auto l = NOver2 - k;
 
         // Extract the FFT of the even components
-        auto A = std::complex<double>(
+        const auto A = std::complex<double>(
             (X_p[k].real() + X_p[l].real()) / 2,
             (X_p[k].imag() - X_p[l].imag()) / 2);
 
         // Extract the FFT of the odd components
-        auto B = std::complex<double>(
+        const auto B = std::complex<double>(
             (X_p[l].imag() + X_p[k].imag()) / 2,
             (X_p[l].real() - X_p[k].real()) / 2);
 
         // Sum the results and take advantage of symmetry
-        auto tmp = W[k] * B;
+        const auto tmp = W[k] * B;
         X[k] = A + tmp;
         X[k + NOver2] = A - tmp;
     }
@@ -76,18 +79,19 @@ std::vector<std::complex<double>> FFT(const std::vector<double>& x)
 
 std::vector<std::complex<double>> FFT(const std::vector<std::complex<double>>& x)
 {
-    size_t N = x.size();
+    const size_t N = x.size();
 
     // Radix2 FFT requires length of the input signal to be a power of 2
     // TODO: Implement other algorithms for when N is not a power of 2
     assert(IsPowerOf2(N));
-    size_t NOver2 = N / 2;
+    const size_t NOver2 = N / 2;
 
     // Pre-calculate twiddle factors
     std::vector<std::complex<double>> W(NOver2);
+    const auto twiddleConstant = -2.0 * M_PI / N;
     for (size_t k = 0; k < NOver2; ++k)
     {
-        W[k] = std::polar(1.0, -2 * M_PI * k / N);
+        W[k] = std::polar(1.0, k * twiddleConstant);
     }
 
     return FFTRadix2(x, W);
@@ -95,20 +99,24 @@ std::vector<std::complex<double>> FFT(const std::vector<std::complex<double>>& x
 
 static std::vector<std::complex<double>> FFTRadix2(const std::vector<std::complex<double>>& x, const std::vector<std::complex<double>>& W)
 {
-    size_t N = x.size();
+    const size_t N = x.size();
 
     // Radix2 FFT requires length of the input signal to be a power of 2
     assert(IsPowerOf2(N));
-    size_t NOver2 = N / 2;
+    const size_t NOver2 = N / 2;
 
     // Calculate how many stages the FFT must compute
-    size_t stages = static_cast<size_t>(log2(N));
+    const size_t stages = static_cast<size_t>(log2(N));
 
     // Pre-load the output vector with the input data using bit-reversed indexes
     std::vector<std::complex<double>> X(N);
+    size_t nReversed = 0;
     for (size_t n = 0; n < N; ++n)
     {
-        X[n] = x[ReverseBits(n, stages)];
+        X[n] = x[nReversed];
+        const size_t change = n ^ (n + 1);
+        const std::bitset<wordSize> bits(~change);
+        nReversed ^= change << (bits.count() - (wordSize - stages));
     }
 
     // Calculate the FFT one stage at a time and sum the results
@@ -121,10 +129,10 @@ static std::vector<std::complex<double>> FFTRadix2(const std::vector<std::comple
         {
             for (size_t n = 0; n < NPreviousStage; ++n)
             {
-                auto index1 = k + n;
-                auto index2 = index1 + NPreviousStage;
-                auto tmp1 = X[index1];
-                auto tmp2 = W[n * WOffset] * X[index2];
+                const auto index1 = k + n;
+                const auto index2 = index1 + NPreviousStage;
+                const auto tmp1 = X[index1];
+                const auto tmp2 = W[n * WOffset] * X[index2];
                 X[index1] = tmp1 + tmp2;
                 X[index2] = tmp1 - tmp2;
             }
@@ -141,18 +149,6 @@ static std::vector<std::complex<double>> FFTRadix2(const std::vector<std::comple
 static bool IsPowerOf2(size_t x)
 {
     return x && (!(x & (x - 1)));
-}
-
-// Given x composed of n bits, returns x with the bits reversed
-static size_t ReverseBits(const size_t x, const size_t n)
-{
-    size_t xReversed = 0;
-    for (size_t i = 0; i < n; ++i)
-    {
-        xReversed = (xReversed << 1U) | ((x >> i) & 1U);
-    }
-
-    return xReversed;
 }
 
 } /* namespace opendsp */
